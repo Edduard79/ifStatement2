679f5c75765d58532d9e3bce4db6811cd14211b8
Eduard <eduard.prog22@gmail.com>
2023-01-20 18:25:24 +0100

    Second try

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,14 @@ public class Main {
 
         String mySurname = "Paolini";
         String mySurname2 = "Rossi";
-        String var1, var2;
+
 
         //variable = (condition) ? expressionTrue :  expressionFalse;
 
-        var1 =(mySurname.contains("ni")) ? "Your surname contains the sequence of letters 'ni'!" : "Your surname doesn't contain] the sequence of letters 'ni'!";
-        System.out.println(mySurname + " " + var1);
+        System.out.println(mySurname + " Your surname " + (mySurname.contains("ni") ? "CONTAINS" : "DOES NOT CONTAIN") + " the sequence of letters 'ni'");
 
-        var2 =(mySurname2.contains("ni")) ? "Your surname contains the sequence of letters 'ni'!" : "Your surname doesn't contain] the sequence of letters 'ni'!";
-        System.out.println(mySurname2 + " " + var2);
+        System.out.println(mySurname2 + " Your surname "  + (mySurname2.contains("ni") ? "contains" : "doesn't contain") + " the sequence of letters 'ni'");
+
 
     }
 }
